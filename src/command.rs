84d2d27bf3f385ba36f45ceda94f46fbeb81c4a5
AlephCubed <jothebro500@gmail.com>
2025-12-18 15:12:54 -0800

    Added (questionable) support for new spawn syntax.

--- src/command.rs
+++ src/command.rs
@@ -1,6 +1,8 @@
 use crate::bundle::EffectBundle;
 use crate::{Delay, EffectMode, EffectTimer, EffectedBy, Effecting, Lifetime};
 use bevy_ecs::prelude::*;
+use bevy_ecs::ptr::MovingPtr;
+use bevy_ecs::spawn::SpawnableList;
 
 /// Applies an effect to a target entity.
 /// This *might* spawn a new entity, depending on what effects are already applied to the target.
@@ -94,6 +96,17 @@ impl<B: Bundle> Command for AddEffectCommand<B> {
     }
 }
 
+impl<B: Bundle> SpawnableList<Effecting> for EffectBundle<B> {
+    fn spawn(this: MovingPtr<'_, Self>, world: &mut World, target: Entity) {
+        let bundle = this.read();
+        world.commands().queue(AddEffectCommand { target, bundle });
+    }
+
+    fn size_hint(&self) -> usize {
+        1
+    }
+}
+
 /// Uses commands to apply effects to a specific target entity.
 pub struct EffectSpawner<'a> {
     target: Entity,
@@ -134,5 +147,117 @@ impl EffectCommandsExt for EntityCommands<'_> {
             commands: &mut self.commands(),
         });
         self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[derive(Component, Debug, Eq, PartialEq, Default)]
+    struct MyEffect(u8);
+
+    #[test]
+    fn spawnable_list_stack() {
+        let mut world = World::new();
+
+        world.spawn((
+            Name::new("Target"),
+            EffectedBy::spawn((
+                EffectBundle {
+                    bundle: MyEffect(0),
+                    ..Default::default()
+                },
+                EffectBundle {
+                    bundle: MyEffect(1),
+                    ..Default::default()
+                },
+            )),
+        ));
+
+        world.flush();
+
+        let effects: Vec<u8> = world
+            .query::<&MyEffect>()
+            .iter(&mut world)
+            .map(|c| c.0)
+            .collect();
+
+        assert!(effects.contains(&0));
+        assert!(effects.contains(&1));
+    }
+
+    #[test]
+    fn spawnable_list_replace() {
+        let mut world = World::new();
+
+        world.spawn((
+            Name::new("Target"),
+            EffectedBy::spawn((
+                EffectBundle {
+                    mode: EffectMode::Replace,
+                    bundle: MyEffect(0),
+                    ..Default::default()
+                },
+                EffectBundle {
+                    mode: EffectMode::Replace,
+                    bundle: MyEffect(1),
+                    ..Default::default()
+                },
+            )),
+        ));
+
+        world.flush();
+
+        let effects: Vec<u8> = world
+            .query::<&MyEffect>()
+            .iter(&mut world)
+            .map(|c| c.0)
+            .collect();
+
+        assert!(!effects.contains(&0));
+        assert!(effects.contains(&1));
+    }
+
+    #[test]
+    fn spawnable_list_mixed() {
+        let mut world = World::new();
+
+        world.spawn((
+            Name::new("Target"),
+            EffectedBy::spawn((
+                EffectBundle {
+                    bundle: MyEffect(0),
+                    ..Default::default()
+                },
+                EffectBundle {
+                    bundle: MyEffect(1),
+                    ..Default::default()
+                },
+                EffectBundle {
+                    mode: EffectMode::Replace,
+                    bundle: MyEffect(2),
+                    ..Default::default()
+                },
+                EffectBundle {
+                    mode: EffectMode::Replace,
+                    bundle: MyEffect(3),
+                    ..Default::default()
+                },
+            )),
+        ));
+
+        world.flush();
+
+        let effects: Vec<u8> = world
+            .query::<&MyEffect>()
+            .iter(&mut world)
+            .map(|c| c.0)
+            .collect();
+
+        assert!(effects.contains(&0));
+        assert!(effects.contains(&1));
+        assert!(!effects.contains(&2));
+        assert!(effects.contains(&3));
     }
 }
